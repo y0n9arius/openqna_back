ff6bf610f550bd01932c1b10e8e149d3d20be2d3
matthew <kimmulgae@lgcns.com>
2022-05-22 15:08:07 +0900

    functions 타입 추가

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,36 @@
-import * as functions from "firebase-functions";
+import { Response } from 'express'
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp(functions.config().firebase);
+
+const db = admin.firestore();
+
+exports.setChannel = functions.https.onRequest(async (response: Response) => {
+  const channelCode = Math.random().toString(16).substring(2, 8);
+  const isDuplicateCode = await db.collection('channel').where('code', '==', channelCode).get().exists
+  if (isDuplicateCode) {
+    response.send("채널을 생성할 수 없습니다.")
+  }
+
+  const data = {
+    name: 'ChannelName' + channelCode,
+    code: channelCode,
+    created_date: Date.now()
+  }
+
+  await db.collection('channel').doc().set(data);
+  response.send(channelCode);
+});
+
+exports.getChannel = functions.https.onRequest(async (response: Response) => {
+  response.set('Access-Control-Allow-Origin', '*');
+
+  const channels = await db.collection('channel').get()
+  const channelNames: any[] = []
+  channels.forEach((doc: any) => {
+    channelNames.push(doc.data())
+  })
+  response.send(channelNames)
+});
